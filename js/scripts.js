c0f703908f42203624ae9a8140eb469d9fe884fc
Brunopeace <137825826+Brunopeace@users.noreply.github.com>
2025-02-05 23:30:50 -0300

    Atualizar o scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,30 +7,36 @@ if ("serviceWorker" in navigator) {
 let deferredPrompt;
 const installButton = document.getElementById("install-button");
 
-// Monitora o evento `beforeinstallprompt`
+// Monitora o evento beforeinstallprompt
 window.addEventListener("beforeinstallprompt", (e) => {
   e.preventDefault();
   deferredPrompt = e;
-  installButton.style.display = "flex"; // Exibe o botão de instalação
+
+  // Exibe o botão de instalação
+  installButton.style.display = "block"; 
 
   installButton.addEventListener("click", async () => {
-    if (deferredPrompt) {
-      installButton.style.display = "none"; // Esconde o botão
-      deferredPrompt.prompt(); // Mostra o prompt de instalação
+    installButton.style.display = "none"; // Esconde o botão
+    deferredPrompt.prompt(); // Mostra o prompt de instalação
 
-      const { outcome } = await deferredPrompt.userChoice;
-      if (outcome === "accepted") {
-        console.log("Usuário aceitou instalar o PWA.");
-      } else {
-        console.log("Usuário recusou a instalação do PWA.");
-      }
-      deferredPrompt = null;
+    const { outcome } = await deferredPrompt.userChoice;
+    if (outcome === "accepted") {
+      console.log("Usuário aceitou instalar o PWA.");
+    } else {
+      console.log("Usuário recusou a instalação do PWA.");
     }
+    deferredPrompt = null;
   });
 });
 
-// Oculta o botão quando o app for instalado
+// Oculta o botão se o app já estiver instalado
 window.addEventListener("appinstalled", () => {
   console.log("Aplicativo foi instalado com sucesso!");
   installButton.style.display = "none";
 });
+
+// Verifica se o PWA já está instalado
+if (window.matchMedia("(display-mode: standalone)").matches) {
+  console.log("O aplicativo já está instalado.");
+  installButton.style.display = "none";
+}
