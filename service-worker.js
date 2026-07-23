c0cd29761625802aa4c342e76087a33a130859b5
Brunopeace <137825826+Brunopeace@users.noreply.github.com>
2026-07-23 11:36:21 -0300

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "union-apps-cache-v9"; // Alterar versão do cache ao atualizar
+const CACHE_NAME = "union-apps-cache-v10"; // Alterar versão do cache ao atualizar
 const urlsToCache = [
   "/",
   "/index.html",
